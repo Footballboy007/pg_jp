84e84a5e7f376ac7292e88386b9b1764d7e8acb5
Footballboy007 <33520725+Footballboy007@users.noreply.github.com>
2018-01-11 10:09:27 -0500

    Delete pg_JP.py

--- pg_JP.py
+++
@@ -1,9 +0,0 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> import pyatogui as pg
-Traceback (most recent call last):
-  File "<pyshell#0>", line 1, in <module>
-    import pyatogui as pg
-ModuleNotFoundError: No module named 'pyatogui'
->>> import pyautogui as pg
->>> 
